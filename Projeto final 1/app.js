db2e6e9495a6ba3b8f0ce3c157579eafb8fec74f
Rayssa Vitória <rayssa.santos121@etec.sp.gov.br>
2025-12-05 16:20:57 -0300

    Update app.js

--- Projeto final 1/app.js
+++ Projeto final 1/app.js
@@ -6,17 +6,15 @@ formulario.addEventListener('submit', (evento) => { //arrow function ou função
     let nome = document.getElementById('nome').value;
     let email = document.getElementById('email').value;
     let senha = document.getElementById('senha').value;
-    //let bolo = document.getElementById('bolo').value;
-    //let suco = document.getElementById('suco').value;
-    //let pagamento = document.getElementById('pagamento').value;
+    let bolo = document.getElementById('bolo').value;
+    let suco = document.getElementById('suco').value;
 
-    console.log(`Nome: ${nome} email: ${email} senha: ${senha} bolo: ${bolo} suco: ${suco} pagamento: ${pagamento}`);
+    console.log(`Nome: ${nome} email: ${email} senha: ${senha} bolo: ${bolo} suco: ${suco}`);
     resultado.innerHTML = ` <p>Nome: ${nome}</p>
                             <p>Email: ${email}</p>
                             <p>Senha: ${senha} </p>
                             <p>Sabor do Bolo: ${bolo} </p>
-                            <p>Bebida: ${suco} </p>
-                            <p>Forma de Pagamento: ${pagamento} </p>`;
+                            <p>Bebida: ${suco} </p>`;
 
 
 }) //escutador de eventos
